ea00d4ec85d365d438ff4155539c71502cc712a5
Rajvi Patel <97229743+Rajvi0321@users.noreply.github.com>
2022-07-27 02:15:06 +0530

    Add files via upload

---
+++ AreaOfCircle.py
@@ -0,0 +1,10 @@
+# Finding area of the circle.
+
+# Taking input from user
+radius = float(input("Enter radius of the circle : "))
+
+# calculating are of circle and rounding it to two decimal place
+Area = 3.14159*radius*radius
+
+# printing the area
+print("Area of a circle with radius ", radius, " is ", Area, "unit sq.")
